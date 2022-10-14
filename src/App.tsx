219b11da03e4053c7d719d7a087cd68ac0879e8f
NicolasMoraesDeSouza <seilanicolas05@gmail.com>
2022-10-14 01:25:21 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './styles/global.css'
+
+export function App() {
+
+
+  return (
+   <h1 className='font-black text-5xl text-purple-400'>Hello World</h1>
+  )
+}
+
