fed42a451b962f6dc720ebb3ef2048f65f0944f6
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2022-10-17 09:24:05 -0300

    Adicionando fonte Inter

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ export function App() {
 
 
   return (
-   <h1 className='font-black text-5xl text-purple-400'>Hello World</h1>
+    <div>
+
+      <h1 className='font-black text-5xl text-purple-400'>Hello World</h1>
+      <button className='bg-green-500 font-medium px-4 py-2 rounded text-black hover:bg-red-500'>Botão paia</button>
+    </div>
+
   )
 }
 
