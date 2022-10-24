7c0319a2f683262ca300123c56be52d9eae6e330
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2022-10-24 04:00:48 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  vitefinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION'){
+        config.base= '/react-design-system'
+
+    }
+    return config
   }
 }
